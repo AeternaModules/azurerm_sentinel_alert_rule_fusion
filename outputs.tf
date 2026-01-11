a859bd3af5ea0bd670be397b7fd486b027c46573
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:03:36 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_alert_rule_fusions" {
+  description = "All sentinel_alert_rule_fusion resources"
+  value       = azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions
+}
+
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "sentinel_alert_rule_fusions" {
+  description = <<EOT
+Map of sentinel_alert_rule_fusions, attributes below
+Required:
+    - alert_rule_template_guid
+    - log_analytics_workspace_id
+Optional:
+    - enabled
+    - name
+    - source (block):
+        - enabled (optional)
+        - name (required)
+        - sub_type (optional, block):
+            - enabled (optional)
+            - name (required)
+            - severities_allowed (required)
+EOT
+
+  type = map(object({
+    alert_rule_template_guid   = string
+    log_analytics_workspace_id = string
+    enabled                    = optional(bool, true)
+    name                       = optional(string, "BuiltInFusion")
+    source = optional(object({
+      enabled = optional(bool, true)
+      name    = string
+      sub_type = optional(object({
+        enabled            = optional(bool, true)
+        name               = string
+        severities_allowed = set(string)
+      }))
+    }))
+  }))
+}
+
