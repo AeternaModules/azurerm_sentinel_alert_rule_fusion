7c7d883f248627a9cc372674f18650a53f3b4565
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "sentinel_alert_rule_fusions" {
-  description = "All sentinel_alert_rule_fusion resources"
-  value       = azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions
-}
 output "sentinel_alert_rule_fusions_alert_rule_template_guid" {
-  description = "List of alert_rule_template_guid values across all sentinel_alert_rule_fusions"
-  value       = [for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : v.alert_rule_template_guid]
+  description = "Map of alert_rule_template_guid values across all sentinel_alert_rule_fusions, keyed the same as var.sentinel_alert_rule_fusions"
+  value       = { for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : k => v.alert_rule_template_guid }
 }
 output "sentinel_alert_rule_fusions_enabled" {
-  description = "List of enabled values across all sentinel_alert_rule_fusions"
-  value       = [for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : v.enabled]
+  description = "Map of enabled values across all sentinel_alert_rule_fusions, keyed the same as var.sentinel_alert_rule_fusions"
+  value       = { for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : k => v.enabled }
 }
 output "sentinel_alert_rule_fusions_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_alert_rule_fusions"
-  value       = [for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_alert_rule_fusions, keyed the same as var.sentinel_alert_rule_fusions"
+  value       = { for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : k => v.log_analytics_workspace_id }
 }
 output "sentinel_alert_rule_fusions_name" {
-  description = "List of name values across all sentinel_alert_rule_fusions"
-  value       = [for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : v.name]
+  description = "Map of name values across all sentinel_alert_rule_fusions, keyed the same as var.sentinel_alert_rule_fusions"
+  value       = { for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : k => v.name }
 }
 output "sentinel_alert_rule_fusions_source" {
-  description = "List of source values across all sentinel_alert_rule_fusions"
-  value       = [for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : v.source]
+  description = "Map of source values across all sentinel_alert_rule_fusions, keyed the same as var.sentinel_alert_rule_fusions"
+  value       = { for k, v in azurerm_sentinel_alert_rule_fusion.sentinel_alert_rule_fusions : k => v.source }
 }
 
